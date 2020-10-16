8898eb1d523b7d93e2152b402a90e838d46b3509
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-10-16 15:22:15 -0500

    wireframed out the 4 route handlers

--- src/app.js
+++ src/app.js
@@ -15,9 +15,71 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+// Authorization middleware
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    // log statement when this failure happens
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+
+
+
+
+
+
+
+app.get('/bookmarks', (req, res) => {
+  res.send('returns a list of bookmarks');
+});
+
+app.get('/bookmarks/:id', (req, res) => {
+  const { id } = req.params;
+  // check if id is a number first, THEN check if it matches existing Ids
+  res.send(`returns a single bookmark with the given ID: ${id} `);
+});
+
+app.post('/bookmarks', (req, res) => {
+  // validate book mark
+  // generate a UUID
+  // add to bookmarks list
+  res.send('accepts a JSON object representing a bookmark and adds it to the list of bookmarks after validation');
+});
+
+app.delete('/bookmarks/:id', (req, res) => {
+  const { id } = req.params;
+  // validate book mark
+  // add to bookmarks list
+  res.send(`deletes the bookmark with the given ID: ${id}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.use((error, req, res, next) => {
   let response;
