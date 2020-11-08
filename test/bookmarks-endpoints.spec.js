5b681b7ce562caeffc3d28f4596732254c2615d5
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-11-08 03:00:55 -0600

    basic tests passing with fixture test data

---
+++ test/bookmarks-endpoints.spec.js
@@ -0,0 +1,65 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const app = require('../src/app');
+const { makeBookmarksArray } = require('./bookmarks.fixtures');
+
+describe('Bookmarks Endpoints', function () {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+    // There's one problem here, the app is the express instance we create in src/app.js. This express instance expects the req.app.get('db') to return the Knex instance. As we're running tests, we skipped the ./src/server.js file, so the .get('db') call won't work! We need to app.set('db', knexInstance) inside our tests! Let's do this at the start in the before block:
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('clean the table', () => db('bookmarks').truncate());
+
+  afterEach('cleanup', () => db('bookmarks').truncate());
+
+
+  describe(`GET /bookmarks`, () => {
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = makeBookmarksArray();
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      });
+
+      it('responds with 200 and all of the bookmarks', () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, testBookmarks)
+      });
+    });
+  });
+
+  describe(`GET /bookmarks/:id`, () => {
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = makeBookmarksArray();
+
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      });
+
+      it('responds with 200 and the specified bookmark', () => {
+        const bookmarkId = 2;
+        const expectedBookmark = testBookmarks[bookmarkId - 1];
+        return supertest(app)
+          .get(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, expectedBookmark)
+      });
+    });
+  });
+
+});
