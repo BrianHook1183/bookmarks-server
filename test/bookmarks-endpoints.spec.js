07bd82c332660dbbd0ffd4baf9ca42b87870b047
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-11-08 03:26:56 -0600

    tests for empty tables / no data are passing

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -23,6 +23,14 @@ describe('Bookmarks Endpoints', function () {
 
 
   describe(`GET /bookmarks`, () => {
+    context(`Given no bookmarks`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, [])
+      });
+    });
     context('Given there are bookmarks in the database', () => {
       const testBookmarks = makeBookmarksArray();
 
@@ -42,6 +50,15 @@ describe('Bookmarks Endpoints', function () {
   });
 
   describe(`GET /bookmarks/:id`, () => {
+    context(`Given no bookmarks`, () => {
+      it(`responds with 404`, () => {
+        const bookmarkId = 123456
+        return supertest(app)
+          .get(`/bookmarks/${bookmarkId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, { error: { message: `Bookmark Not Found` } })
+      });
+    });
     context('Given there are bookmarks in the database', () => {
       const testBookmarks = makeBookmarksArray();
 
